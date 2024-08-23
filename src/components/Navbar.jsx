a5e6eda71254d9cda8a54022e3c48fac6a591927
Rits113116 <rr4618634@gmail.com>
2024-08-23 15:38:23 +0530

    add loading effect using nextjs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import NavbarItem from './NavbarItem';
-
+import { Suspense } from 'react';
 export default function Navbar() {
   return (
-    <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
+    <div className='flex dark:bg-gray-600 bg-purple-100 p-4 lg:text-lg justify-center gap-6'>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavbarItem title='Trending' param='fetchTrending' />
       <NavbarItem title='Top Rated' param='fetchTopRated' />
-    </div>
+    </Suspense>
+    </div>
   );
 }
